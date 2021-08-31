5df140b4bd811882c73ff551595e7798507724ce
Herman Wu <huanmingwu@msn.com>
2021-08-31 09:56:25 +0800

    split device_cl project

--- src/device.cpp
+++ src/device.cpp
@@ -1,7 +1,17 @@
 #include "device.h"
-#include "device_cl.h"
+#ifdef USE_OPENCL
+#include "opencl/device_cl.h"
+#endif
+#ifdef USE_CUDA
+#include "cuda/device_cu.h"
+#endif
 
 void Device::enum_devices(std::vector<Device*> &list)
 {
+#ifdef USE_OPENCL
     DeviceCl::enum_cl_devices(list);
+#endif
+#ifdef USE_CUDA
+    DeviceCu::enum_cu_devices(list);
+#endif
 }
